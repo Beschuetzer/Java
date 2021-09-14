a48efb7265baadc4a962b15d2b1748d7004b44e2
Adam <adam.j.major@gmail.com>
2021-09-14 07:46:51 -0500

    Start final OOP section project

---
+++ Polymorphism/src/major/adam/Mitsubishi.java
@@ -0,0 +1,22 @@
+package major.adam;
+
+public class Mitsubishi extends Car {
+    public Mitsubishi(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Mitsubishi => starting engine...";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Mitsubishi => accelerating...";
+    }
+
+    @Override
+    public String brake() {
+        return "Mitsubishi => braking...";
+    }
+}
