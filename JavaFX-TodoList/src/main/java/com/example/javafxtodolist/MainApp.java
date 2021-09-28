f8380c9376f92fc9c41a9a4d289308268f7185b7
Adam <adam.j.major@gmail.com>
2021-09-27 20:53:19 -0500

    Add basic io for mocking data retrieval

--- JavaFX-TodoList/src/main/java/com/example/javafxtodolist/MainApp.java
+++ JavaFX-TodoList/src/main/java/com/example/javafxtodolist/MainApp.java
@@ -1,5 +1,6 @@
 package com.example.javafxtodolist;
 
+import com.example.javafxtodolist.dataModel.TodoData;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,6 +9,15 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class MainApp extends Application {
+    @Override
+    public void init() throws Exception {
+        try {
+            TodoData.getInstance().loadToDoItems();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("hello-view.fxml"));
@@ -19,5 +29,15 @@ public class MainApp extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        try {
+            TodoData.getInstance().storeTodoItems();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
