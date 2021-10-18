54ea06b13b21d066326092c1e1019c3dd50b1750
Adam <adam.j.major@gmail.com>
2021-10-17 20:01:49 -0500

    Finish JUnit testing section challenges

--- JUnit Testing-Challenges/src/major/adam/Utilties.java
+++ JUnit Testing-Challenges/src/major/adam/Utilties.java
@@ -4,15 +4,24 @@ public class Utilties {
     public char[] everyNthChar(char[] sourceArray, int n) {
         //return char array containing every nth char
         if (n <= 0) throw new IllegalArgumentException("n must be > 0");
-        if (sourceArray == null || sourceArray.length == 0) throw new IllegalArgumentException("sourceArray must be at least length 1");
+        if (sourceArray == null || sourceArray.length == 0)
+            throw new IllegalArgumentException("sourceArray must be at least toReturnLength 1");
         if (sourceArray.length < n) throw new IllegalArgumentException("sourceArray doesn't have n number of items");
 
-        char[] toReturn = new char[(int) Math.ceil(sourceArray.length / n)];
+        double toReturnLength = Math.floor((double) sourceArray.length / (double) n);
+        char[] toReturn = new char[(int) toReturnLength];
         int toReturnIndex = 0;
-        for (int i = 0; i < sourceArray.length; i+=n) {
+
+        for (int i = 0; i < sourceArray.length; i++) {
             if (toReturnIndex >= toReturn.length) continue;
-            toReturn[toReturnIndex] = sourceArray[i];
-            toReturnIndex++;
+            if ((i + 1) % n == 0) {
+                toReturn[toReturnIndex] = sourceArray[i];
+                toReturnIndex++;
+            }
+        }
+
+        for (char c : toReturn) {
+            System.out.println(c);
         }
 
         return toReturn;
@@ -21,25 +30,26 @@ public class Utilties {
     public String removePairs(String source) {
         //remove repeating characters (e.g. 'eeaaaafbcda' => 'eafbcda')
         if (source == null) return "";
-        if (source.length() == 0) return "";
-        if (source.length() == 1) return source;
+        if (source.length() < 2) return source;
 
         StringBuilder sb = new StringBuilder();
         char[] sourceChars = source.toCharArray();
-        for(int i = 0; i < sourceChars.length - 1; i++) {
+        for (int i = 0; i < sourceChars.length - 1; i++) {
             char currentChar = sourceChars[i];
             char nextChar = sourceChars[i + 1];
             if (currentChar == nextChar) {
+                if (i == sourceChars.length - 2) sb.append(nextChar);
                 continue;
             }
-            sb.append(currentChar);
-        }
 
+            sb.append(currentChar);
+            if (i == sourceChars.length - 2) sb.append(nextChar);
+        }
         return sb.toString();
     }
 
     public int converter(int a, int b) {
-        return (a/b) + (a * 30) - 2;
+        return (a / b) + (a * 30) - 2;
     }
 
     public String nullIfOddLength(String source) {
--- JUnit Testing-Challenges/src/major/adam/UtiltiesTestParameterized.java
+++ JUnit Testing-Challenges/src/major/adam/UtiltiesTestParameterized.java
@@ -25,55 +25,80 @@ class UtiltiesTestParameterized {
 
     @ParameterizedTest
     @MethodSource
-    void everyNthChar_valid(char[] sourceArray, int n, char[] expected) {
-        assertArrayEquals(instance.everyNthChar(sourceArray, n), expected);
+    void everyNthChar_valid(char[] sourceArray, int n, Object expected) {
+        if (expected instanceof char[]) {
+            assertArrayEquals(instance.everyNthChar(sourceArray, n), (char[])expected);
+            return;
+        } else {
+            assertThrows((Class) expected, () -> {
+                instance.everyNthChar(sourceArray, n);
+            });
+        }
     }
 
     @ParameterizedTest
     @MethodSource
-    void removePairs_valid(String source) {
-        fail("Implement me");
+    void removePairs_valid(String source, String expected) {
+        assertEquals(expected, instance.removePairs(source));
     }
 
     @ParameterizedTest
     @MethodSource
-    void converter_valid(int a, int b) {
-        fail("Implement me");
+    void converter_valid(int a, int b, Object expected) {
+        System.out.println(expected);
+        if (expected instanceof Integer) {
+            System.out.println(1);
+            assertEquals(instance.converter(a, b), (int) expected);
+            return;
+        } {
+            assertThrows((Class) expected, () -> {
+                instance.converter(a, b);
+            });
+            System.out.println(2);
+        }
     }
 
     @ParameterizedTest
     @MethodSource
-    void nullIfOddLength_valid(String source) {
-        fail("Implement me");
+    void nullIfOddLength_valid(String source, String expected) {
+        assertEquals(instance.nullIfOddLength(source), expected);
     }
 
     public static Stream<Arguments> everyNthChar_valid() {
         return Stream.of(
                 Arguments.of(new char[] {'a', 'b', 'c','d','e','f','g'}, 1, new char[]{'a', 'b', 'c','d','e','f','g'} ),
                 Arguments.of(new char[] {'a', 'b', 'c','d','e','f','g'}, 2, new char[]{'b','d','f'} ),
-                Arguments.of(new char[] {'a', 'b', 'c','d','e','f','g'}, 3, new char[]{'c','f'} )
+                Arguments.of(new char[] {'a', 'b', 'c','d','e','f','g'}, 3, new char[]{'c','f'} ),
+                Arguments.of(new char[] {'h', 'e', 'l','l','o'}, 2, new char[]{'e','l'} ),
+                Arguments.of(new char[] {'a', 'b', 'c','d','e','f','g'}, 0, IllegalArgumentException.class ),
+                Arguments.of(null, 2, IllegalArgumentException.class ),
+                Arguments.of(null, 4, IllegalArgumentException.class )
         );
     }
 
     public static Stream<Arguments> removePairs_valid() {
         return Stream.of(
-                Arguments.of(),
-                Arguments.of(),
-                Arguments.of()
+                Arguments.of("AABCDDEFF", "ABCDEF"),
+                Arguments.of("ABCCABDEEF", "ABCABDEF"),
+                Arguments.of("ABCDEFF", "ABCDEF"),
+                Arguments.of("AB88EFFG", "AB8EFG"),
+                Arguments.of("112233445566", "123456"),
+                Arguments.of("ZYZQQB", "ZYZQB"),
+                Arguments.of("", ""),
+                Arguments.of("A", "A"),
+                Arguments.of(null, "")
         );
     }
     public static Stream<Arguments> converter_valid() {
         return Stream.of(
-                Arguments.of(),
-                Arguments.of(),
-                Arguments.of()
+                Arguments.of(10, 5, 300),
+                Arguments.of(10, 0, ArithmeticException.class)
         );
     }
     public static Stream<Arguments> nullIfOddLength_valid() {
         return Stream.of(
-                Arguments.of(),
-                Arguments.of(),
-                Arguments.of()
+                Arguments.of("ABCD", "ABCD"),
+                Arguments.of("ABC", null)
         );
     }
 }
