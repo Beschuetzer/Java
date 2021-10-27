6bd270b3f996177137ac899d73c1e5c4bf37e99b
Adam <adam.j.major@gmail.com>
2021-10-27 09:43:06 -0500

    Start Spring Boot course

---
+++ Spring Boot Demo/src/test/java/com/adammajor/springbootdemo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adammajor.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
